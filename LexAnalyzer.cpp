1ddfd5417fa066e0c4ea4027ab029b859f383206
AlejandroN176 <147194662+AlejandroN176@users.noreply.github.com>
2025-03-17 11:42:41 -0500

    fixed file placements and replaced with evans code

--- LexAnalyzer.cpp
+++ LexAnalyzer.cpp
@@ -1,4 +1,3 @@
-
 #include "LexAnalyzer.h"
 // pre: parameter refers to open data file consisting of token and
 // lexeme pairs i.e.  s_and and t_begin begin t_int 27
@@ -6,11 +5,19 @@
 // post: tokenmap has been populated - key: lexeme, value: token
 
 LexAnalyzer::LexAnalyzer(istream& infile) {
-    string token;
-    string lexeme;
-    while (infile >> token >> lexeme) {
-        tokenmap[lexeme] = token;
-    }
+        if (!infile) {
+                cerr << "Error: Failed to open lexemes.txt!" << endl;
+                return;
+        }
+        string token;
+        string lexeme;
+
+        while (infile >> token >> lexeme) {
+                tokenmap[lexeme] = token;
+                cout << "Added: " << lexeme << " -> " << token << endl;
+        }
+
+
 }
 
 // pre: 1st parameter refers to an open text file that contains source
@@ -22,4 +29,13 @@ LexAnalyzer::LexAnalyzer(istream& infile) {
 // an error message have been written to the output file.
 // A success or fail message has printed to the console.
 void LexAnalyzer::scanFile(istream& infile, ostream& outfile) {
+        string word;
+
+        while (infile >> word) {
+                if (tokenmap.find(word) != tokenmap.end()){
+                        outfile << tokenmap[word] << " : " << word << endl;
+                } else {
+                        outfile << "ERROR: Unrecognized lexeme '" << word << "'" << endl;
+                }
+        }
 }
